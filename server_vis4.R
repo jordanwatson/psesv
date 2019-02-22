e8479dd6d2c2611927f380d71e4cc5dab71718ca
Jordan Watson <jordanwatson@users.noreply.github.com>
2019-02-21 20:27:49 -0900

    responded to reviewers

--- server_vis4.R
+++ server_vis4.R
@@ -1,3 +1,4 @@
+vis <- "vis4"
 source("Global.R")
 
 #-------------------------------------------------------------------------------
@@ -29,46 +30,77 @@ source("Global.R")
 
 simple <- readOGR(dsn="Data",layer="adfg_stat_areas_simple")
 
+# Find a center point for each region
+#centers <- data.frame(gCentroid(simple, byid = TRUE))
+#centers$region <- row.names(simple)
+
+
 server_vis4 <- shinyServer(function(input, output) {
-  data <- reactiveValues(clickedShape=NULL)
+  #data <- reactiveValues(clickedShape=vector())
+  click_list <- reactiveValues(ids=vector())
   # produce the basic leaflet map
   output$map <- renderLeaflet(
+    
+  #labels <- sprintf("<strong>%s</strong>",simple$STAT_AREA) %>% lapply(htmltools::HTML)
+    
+    
     leaflet(simple) %>% 
       addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
                   opacity = 1.0, fillOpacity = 1,
                   fillColor = "blue",
                   highlightOptions = highlightOptions(color = "white", weight = 2,
                                                       bringToFront = TRUE),
-                  layerId = ~STAT_AREA)
+                  layerId = ~STAT_AREA,
+                  label=~STAT_AREA,
+                  labelOptions = labelOptions(style = list("font-weight" = "normal", 
+                                                           padding = "3px 8px"),
+                                              textsize = "15px",
+                                              direction = "auto"
+                  ))
   )
   
   observeEvent(input$map_shape_click, { # update the location selectInput on map clicks
-    data$clickedShape <- input$map_shape_click
-    print(data$clickedShape)
+    #data$clickedShape <- input$map_shape_click
+    click <- input$map_shape_click
+    click_list$ids <- c(click_list$ids, click$id)
+    print(click_list$ids)
   })
   
   #output$myTable <- renderTable({
   #  return(data$clickedMarker$id)
   # observe the marker click info and print to console when it is changed.
   #})
-  output$mainplot <- renderPlot({
-    req(data$clickedShape$id)
-    dataset <- mydata %>% 
-      filter(STAT_AREA==data$clickedShape$id)
-    
-    p <- ggplot(dataset %>% mutate(date=as.POSIXct(date)), aes(x = date, y = sst.mean)) +
+  #output$mainplot <- renderPlot({
+  #  req(data$clickedShape$id)
+  #  dataset <- mydata %>% 
+  #    filter(STAT_AREA%in%data$clickedShape$id)
+
+  observeEvent(input$reset,{
+    click_list$ids <- NULL
+  })
+  
+    output$mainplot <- renderPlot({
+      req(click_list$ids)
+      
+      if(is.null(click_list$ids)) return()
+      
+      dataset <- mydata %>% 
+        filter(STAT_AREA%in%click_list$ids)
+      
+    p <- ggplot(dataset, aes(x = date, y = sst.mean,color=factor(STAT_AREA))) +
       geom_line() +
       theme_bw() + 
-      ggtitle(paste("Daily sea surface temperatures, stat area",data$clickedShape$id)) + 
+      #ggtitle(paste("Daily sea surface temperatures, stat area",click_list$ids)) + 
       scale_x_datetime(date_breaks="1 year",date_labels="%Y") + 
+      scale_color_viridis(discrete=TRUE,
+                          guide=guide_legend("Statistical Area",ncol=4)) + 
       theme(legend.position="top",
             legend.text=element_text(size=14),
             legend.title=element_text(size=14),
             axis.text = element_text(size=10),
             axis.title = element_text(size=14)) + 
       xlab("Date") +
-      ylab("Temperature (°C)")
+      ylab(expression("Temperature " ( degree*C)))
     print(p)
   })
 })
-
