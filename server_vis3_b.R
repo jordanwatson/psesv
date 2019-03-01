8389bfdb975e2b2f6bea70c91e510840e35025f0
Jordan Watson <jordanwatson@users.noreply.github.com>
2019-02-28 15:57:44 -0900

    final

---
+++ server_vis3_b.R
@@ -0,0 +1,109 @@
+vis <- "vis3"
+source("Global.R")
+
+#data2 <- mydata %>% 
+#  mutate(newyr=ifelse(month<4,year-1,year),
+#                        month2=ifelse(month<4,month+12,month),
+#                        season=ifelse(month2%in%c(4:9),"Summer (Apr - Sept)","Winter (Oct - Mar)"),
+#                        monthname=month.name[month]) 
+
+mynmfs <- sort(unique(data2$NMFSAREA))
+
+gg_facet_nrow<- function(p){
+  p %>% ggplot2::ggplot_build()  %>%
+    magrittr::extract2('layout')       %>%
+    magrittr::extract2('panel_layout') %>%
+    magrittr::extract2('ROW')          %>%
+    unique()                           %>%
+    length()
+}
+
+gg_facet_nrow <- function(p){
+  num_panels <- length(unique(ggplot_build(p)$data[[1]]$PANEL)) # get number of panels
+  num_cols <- ggplot_build(p)$layout$facet$params$ncol # get number of columns set by user
+  num_rows <- wrap_dims(num_panels, ncol=num_cols)[1] # determine number of rows
+}
+#----------------------------------------------------
+#  Multi-panel layout
+#----------------------------------------------------
+
+server_vis3 <- shinyServer(function(input, output){
+  ## values <- reactiveValues()  # unused
+  ## Your data should be reactive - and reference `input` 
+  ## to get user-entered values
+  rxDataMonthly <- reactive({
+
+    data2 %>% 
+      filter(m.depth>=input$range_month[1] & m.depth<=input$range_month[2] & NMFSAREA%in%c(input$dnmfs_month) & monthname%in%input$dmonth) %>% 
+      group_by(NMFSAREA,month,year) %>% 
+      summarise(meantemp=mean(sst.mean,na.rm=TRUE)) %>% 
+      ungroup %>% 
+      group_by(NMFSAREA,month) %>% 
+      mutate(tempanom=(meantemp-mean(meantemp,na.rm=TRUE))/sd(meantemp,na.rm=TRUE),
+             monthname=month.name[month])
+  })
+
+  myplot <- reactive({
+    req(input$dnmfs_month)
+    req(input$dmonth)
+    
+    dataset <- rxDataMonthly()  # this is the subsetted data
+    
+    dataset$monthname <- factor(dataset$monthname,levels=unique(dataset$monthname[order(dataset$month)]))
+  
+    myplot <- ggplot(dataset, aes(x = year, y = tempanom, fill=factor(NMFSAREA))) +
+      geom_bar(stat="identity",position="dodge") + 
+      theme_bw() + 
+      scale_fill_viridis(name="NMFS Area",discrete=TRUE) + 
+      xlab("Year") + 
+      ylab("Temperature Anomaly (SD)") + 
+      theme(legend.position="top",
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=14),
+            axis.text = element_text(size=13),
+            axis.title = element_text(size=14),
+            plot.title = element_text(size=16),
+            strip.text = element_text(size=14)) + 
+      scale_x_continuous(breaks = 2003:2018, labels = c("2003","","2005","","2007","","2009","","2011","","2013","","2015","","2017","")) + 
+      facet_wrap(. ~ monthname,ncol=1)
+    return(myplot)
+  })
+  
+  #  Determine number of months in order to scale the size of the facets in the plot
+  he <- reactive(gg_facet_nrow(myplot()))
+  
+  output$monthly_nmfs_plot <- renderPlot({myplot()}, height = function(){he()*250})
+  
+  rxDataSeasonal <- reactive({
+    #req(input$dnmfs)
+    dat <- data2 %>% 
+      filter(m.depth>=input$range[1] & m.depth<=input$range[2] & newyr>2002 & newyr<2018 & NMFSAREA%in%c(input$dnmfs)) %>% 
+      group_by(NMFSAREA,season,newyr) %>% 
+      summarise(meantemp=mean(sst.mean,na.rm=TRUE)) %>% 
+      ungroup %>% 
+      group_by(NMFSAREA,season) %>% 
+      mutate(tempanom=(meantemp-mean(meantemp,na.rm=TRUE))/sd(meantemp,na.rm=TRUE))
+    dat
+  })
+  
+  output$seasonal_nmfs_plot <- renderPlot({
+    req(input$dnmfs)
+    datasetSeasonal <- rxDataSeasonal()  # this is the subsetted data
+    p <- ggplot(datasetSeasonal, aes(x = newyr, y = tempanom, fill=factor(NMFSAREA))) +
+      geom_bar(stat="identity",position="dodge") + 
+      facet_wrap(~season) + 
+      theme_bw() + 
+      scale_fill_viridis(name="NMFS Area",discrete=TRUE) + 
+      xlab("Year") + 
+      ylab("Temperature Anomaly (SD)") + 
+      theme(legend.position="top",
+            legend.text=element_text(size=14),
+            legend.title=element_text(size=14),
+            axis.text = element_text(size=13),
+            axis.title = element_text(size=14),
+            strip.text = element_text(size=14)) + 
+      scale_x_continuous(breaks = 2003:2018, labels = c("2003","","2005","","2007","","2009","","2011","","2013","","2015","","2017",""))
+    print(p)
+  })
+})
+
---
+++ ui_vis3_b.R
@@ -0,0 +1,34 @@
+
+ui_vis3 <- fluidPage(
+  tabsetPanel(
+    tabPanel("NMFS Seasonal Anomaly",
+             titlePanel("Seasonal sea surface temperature anomaly"),
+             p("Select NMFS reporting area(s) to view winter and summer temperature anomalies. October - December of each winter corresponds to the previous year (e.g., winter 2010 includes Oct - Dec 2017 and Jan - Mar 2018). 2018 is omitted from summer anomalies because the data are incomplete. Winter 2003 includes only Jan - Mar 2003 and not Oct - Dec 2002. Depth filters remove statistical areas whose average depth is outside of the selected depth range. The default depth is set at 0 - 200m, a general approximation for the continental shelf. If the selected depth leads to a plotting error try a different depth range."),
+             fluidRow(
+               column(4,
+                      pickerInput("dnmfs","Select NMFS reporting area", choices=mynmfs, options = list(`actions-box` = TRUE),multiple = T)),
+               column(6,
+                      sliderInput("range", "Depth (m):",min = -5000, max = 0,value = c(-200,0))
+               ),
+               plotOutput('seasonal_nmfs_plot')
+             )),
+    tabPanel("NMFS Monthly Anomaly",
+             titlePanel("Monthly sea surface temperature anomaly"),
+             p("Select NMFS reporting area(s) and month to view temperature anomalies (standard deviations) for those areas and that month. Depth filters remove statistical areas whose average depth is outside of the selected depth range. The default depth is set at 0 - 200m, a general approximation for the continental shelf. If the selected depth leads to a plotting error try a different depth range."),
+             fluidRow(
+               #column(4,
+               #        selectInput('dnmfs_month', 'NMFS area', mynmfs, multiple=TRUE)),
+               column(4,
+                      pickerInput("dnmfs_month","Select NMFS reporting area", choices=mynmfs, options = list(`actions-box` = TRUE),multiple = T)),
+               #column(4,
+                #      selectInput('dmonth', 'Month', month.name)),
+               column(4,
+                      pickerInput("dmonth","Select month", choices=month.name, options = list(`actions-box` = TRUE),multiple = T)),
+               column(4,
+                      sliderInput("range_month", "Depth (m):",min = -5000, max = 0,value = c(-200,0))
+               )),
+             plotOutput('monthly_nmfs_plot')
+    )
+    )
+  )
+
